c7fded54b580ff49bb90661b2c77640660f36806
Melvin Cayas <cayasmj@gmail.com>
2021-06-21 10:43:14 -0400

    Updated sounds

--- public/scripts/utilities/sounds.js
+++ public/scripts/utilities/sounds.js
@@ -1,31 +1,31 @@
 const volume = document.getElementById("sound-btn");
 
 const start = new Howl({
-	src: ["/sounds/start.mp3"],
+	src: ["sounds/start.mp3"],
 });
 
 const correct = new Howl({
-	src: ["/sounds/correct.mp3"],
+	src: ["sounds/correct.mp3"],
 });
 
 const incorrect = new Howl({
-	src: ["/sounds/incorrect.mp3"],
+	src: ["sounds/incorrect.mp3"],
 });
 
 const timer = new Howl({
-	src: ["/sounds/timer.mp3"],
+	src: ["sounds/timer.mp3"],
 });
 
 const menu = new Howl({
-	src: ["/sounds/menu.mp3"],
+	src: ["sounds/menu.mp3"],
 });
 
 const quit = new Howl({
-	src: ["/sounds/quit.mp3"],
+	src: ["sounds/quit.mp3"],
 });
 
 const error = new Howl({
-	src: ["/sounds/error.mp3"],
+	src: ["sounds/error.mp3"],
 });
 
 volume.addEventListener("click", () => {
